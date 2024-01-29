0e52d15652fb1d4a29c8bcadb990d96b072e1732
Кристина <kristina.shtaynmets@mail.ru>
2024-01-29 23:54:58 +0700

    Практическая 7

--- vue/js/main.js
+++ vue/js/main.js
@@ -3,30 +3,47 @@ let app = new Vue({
     data: {
         product: "Socks",
         brand: 'Vue Mastery',
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
-        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
-        cart: 0
+        cart: 0,
+        onSale: false
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+    },
+    computed: {
+        sale(){
+            return this.onSale ? `${this.brand} ${this.product} РАСПРОДАЖАА` : `${this.brand} ${this.product} Ничего сегодня не распродаёт`;
+        },
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
  })
