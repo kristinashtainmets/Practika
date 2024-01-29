b3eaa193dceb6b521f573579fc3b4a78a1aa6b0b
Кристина <kristina.shtaynmets@mail.ru>
2024-01-29 14:10:04 +0700

    Упростили запись v-bind

--- vue/js/main.js
+++ vue/js/main.js
@@ -2,6 +2,7 @@ let app = new Vue({
    el: '#app',
    data: {
        product: "Socks",
-       image: "./assets/vmSocks-blue-onWhite.jpg"
+       image: "./assets/vmSocks-blue-onWhite.jpg",
+       altText: "A pair of socks"
    }
 })
