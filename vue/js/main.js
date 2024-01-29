9fa90d28e59238d54931bd938e4a978f11f532df
Кристина <kristina.shtaynmets@mail.ru>
2024-01-29 15:06:46 +0700

    Практическая 4

--- vue/js/main.js
+++ vue/js/main.js
@@ -15,8 +15,8 @@ let app = new Vue({
                 variantId: 2235,
                 variantColor: 'blue'
             }
-         ]
-         
+         ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'] 
     }
  })
  
