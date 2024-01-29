33d2fc6d4aa926525ede5604e22095ae1bd6c84c
Кристина <kristina.shtaynmets@mail.ru>
2024-01-29 13:47:34 +0700

    Практическая 1

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,7 +1,7 @@
 let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks"
-    }
- })
- 
+   el: '#app',
+   data: {
+       product: "Socks",
+       description: "A pair of warm, fuzzy socks"
+   }
+})
